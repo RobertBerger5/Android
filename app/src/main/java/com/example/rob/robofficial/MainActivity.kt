9941e55c5c02c96fd14edb3aa884b513b8e5eda4
Rob Berger <robertberger5@gmail.com>
2019-01-11 15:38:26 -0600

    got it to work with Kotlin, it just hates when things aren't initialized...oh also there's colors tahat represent where it's at: ready for speech, listening to speech, or processing/waiting for you to hit the button again...

--- app/src/main/java/com/example/rob/robofficial/MainActivity.kt
+++ app/src/main/java/com/example/rob/robofficial/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rob.robofficial
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
@@ -13,17 +14,18 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.TextView
 
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity(), RecognitionListener {
-    var sr: SpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-    var text: TextView = findViewById<View>(R.id.textMain) as TextView
-    //public Button button;
-    var errors: TextView = findViewById<View>(R.id.textErrors) as TextView
-    var listening = false
-    //var result: ArrayList<String>
+    var sr: SpeechRecognizer? = null
+    var text: TextView? = null
+    var errors: TextView? = null
+    var fab:FloatingActionButton? = null
+    //var listening = false
+    var result: ArrayList<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,20 +33,18 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        //text = findViewById<View>(R.id.textMain) as TextView
+        text = findViewById<View>(R.id.textMain) as TextView
 
-        //sr = SpeechRecognizer.createSpeechRecognizer(this)
-        sr.setRecognitionListener(this)//this activity is the listener
-
-        //startListening();
+        sr = SpeechRecognizer.createSpeechRecognizer(this)
+        sr!!.setRecognitionListener(this)//this activity is the listener
 
 
-        //errors = findViewById<View>(R.id.textErrors) as TextView
+        errors = findViewById<View>(R.id.textErrors) as TextView
 
         //text.setText("nipples");
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            text.text = "end me"
+        fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        fab!!.setOnClickListener { view ->
+            text!!.text = "end me"
             Snackbar.make(view, "how does this work please help", Snackbar.LENGTH_SHORT)
                     .setAction("Action", null).show()
             startListening()//just set the activity for the little email button to do it for now...
@@ -75,7 +75,7 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        text.text = "stop fucking with the options, idiot."
+        text!!.text = "stop fucking with the options, idiot."
         val id = item.itemId
 
 
@@ -86,61 +86,55 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
     }
 
     fun error(text: String) {
-        errors.append(text + "\n")
+        errors!!.append(text + "\n")
     }
 
     override fun onReadyForSpeech(params: Bundle) {
-        println("ready captain")
-        error("onReadyForSpeech")
+        errors!!.setBackgroundColor(Color.GREEN)
     }
 
     override fun onBeginningOfSpeech() {
-        println("started talkin")
-        error("onBeginningOfSpeech")
+        errors!!.setBackgroundColor(Color.CYAN)
     }
 
     override fun onRmsChanged(rmsdB: Float) {
-        println("new dB value: $rmsdB")
         //error("RMS changed to "+rmsdB);
     }
 
     override fun onBufferReceived(buffer: ByteArray) {
-        println("got a buffer...")
-        error("onBufferReceived")
+        //error("onBufferReceived")
     }
 
     override fun onEndOfSpeech() {
-        println("done talkin")
-        error("onEndOfSpeech")
+        errors!!.setBackgroundColor(Color.WHITE)
     }
 
     override fun onError(error: Int) {
         println("ERROR $error")
-        text.text = "ERROR: $error"
+        text!!.text = "ERROR: $error"
         if (error == 9) {
             requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), 1)
-            sr.stopListening()
-            listening = false
-            println("got record audio permission")
+            //sr!!.stopListening()
+            //listening = false
+            println("requested record audio permission")
         }
         if (error == 2) {
             requestPermissions(arrayOf(Manifest.permission.INTERNET), 1)
-            sr.stopListening()
-            listening = false
-            println("got internet access")
+            //sr!!.stopListening()
+            //listening = false
+            println("requested internet access")
         }
         //stopListening();
-        sr.stopListening()
-        listening = false
+        sr!!.stopListening()
+        //listening = false
         println("stopped listening")
         //button.setText("Start Listening");
     }
 
     override fun onResults(results: Bundle) {
         var result: ArrayList<String> = results.get(/*sr.RESULTS_RECOGNITION*/SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
-        println("GOOD SHIT COMIN IN HOT BOYS WATCH TF OUT")
         println(result[0])
-        text.text = result[0]//Main one, the one it's most confident about...
+        text!!.text = result[0]//Main one, the one it's most confident about...
         error("YOU JUST SAID: $result")
         //it accepted it without me hitting "stop listening"
         //when I hit stop listening, it said error 5 because it had already stopped...
@@ -150,7 +144,6 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
     }
 
     override fun onPartialResults(partialResults: Bundle) {
-        println("partial results...?")
         var result: ArrayList<String> = partialResults.get(/*sr.RESULTS_RECOGNITION*/SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String> //copy pasted from above
         error("partial results are in: $result")
     }
@@ -162,9 +155,8 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
     fun startListening() {
         val speechIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        sr.startListening(speechIntent)
-        listening = true
-        println("started listening")
+        sr!!.startListening(speechIntent)
+        //listening = true
         //button.setText("Stop Listening");
         error("started listening")
     }
